f8f5abc4e6b5543f8abb97f0d31c8a0aab5e6a7b
George Mendoza <gsmendoza@gmail.com>
2012-01-19 16:37:05 +0800

    [#8] Remove examples where voices are not grouped by bars.

--- spec/lib/gitara/app_spec.rb
+++ spec/lib/gitara/app_spec.rb
@@ -2,11 +2,18 @@ require 'spec_helper'
 
 describe App do
   describe "export" do
-    it "can convert a gitara file to lilypond" do
+    it "can convert a tab without a reused bar to lilypond" do
       app = Gitara::App.new
-      app.invoke :export, ['examples/tab.gtr'], "target-directory" => test_tmp_dir.path, "run-lilypond" => false
+      app.invoke :export, ['examples/tab-with-bar.gtr'], "target-directory" => test_tmp_dir.path, "run-lilypond" => false
 
-      (test_tmp_dir / 'tab.ly').read.should == Pow('examples/tab.ly').read
+      (test_tmp_dir / 'tab-with-bar.ly').read.should == Pow('examples/tab.ly').read
+    end
+
+    it "can convert a tab with a reused bar to lilypond" do
+      app = Gitara::App.new
+      app.invoke :export, ['examples/tab-with-reused-bar.gtr'], "target-directory" => test_tmp_dir.path, "run-lilypond" => false
+
+      (test_tmp_dir / 'tab-with-reused-bar.ly').read.should == Pow('examples/tab.ly').read
     end
   end
 end
