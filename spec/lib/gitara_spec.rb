ff9a17e2c7ea4c63ac1d69f0e9d8af4e23c8d808
George Mendoza <gsmendoza@gmail.com>
2012-03-27 17:15:12 +0800

    [#24] Document Gitara.

--- spec/lib/gitara_spec.rb
+++ spec/lib/gitara_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe Gitara do
-  describe "tab" do
+  describe ".tab" do
     it "should create a tab instance if it does not exist" do
       Gitara.tab.should be_nil
       Gitara.define
       Gitara.tab.should be_a(Node::Tab)
     end
+  end
 
+  describe ".define" do
     it "should instance evaluate a block if given" do
       tab = Gitara.define do
         bar "Intro" do
